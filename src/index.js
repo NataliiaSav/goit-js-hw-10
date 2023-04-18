fa55b349cc157898a51974aab5e1bf7a1a997a36
NataliiaSav <savchenko6561@gmail.com>
2023-04-18 18:06:35 +0300

    upd index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,25 @@ import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
+function onInputCountry(e) {
+  e.preventDefault();
+  const searchCountry = e.target.value.trim();
+}
+
+fetch(
+  'https://restcountries.com/v3.1/name/deutschland?fields=name,capital,population,flags,languages'
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(name => {
+    console.log(name);
+  })
+  .catch(error => {
+    console.log(error);
+  });
