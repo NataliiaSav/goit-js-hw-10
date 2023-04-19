02828f9143d19701d290de9e601c96e24474b0ef
NataliiaSav <savchenko6561@gmail.com>
2023-04-19 13:37:14 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
@@ -11,17 +12,36 @@ inputEl.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
 function onInputCountry(e) {
   e.preventDefault();
   const searchCountry = e.target.value.trim();
-}
+  if (searchCountry === '') {
+    return (countryInfo.innerHTML = ''), (countryList.innerHTML = '');
+  }
 
-fetch(
-  'https://restcountries.com/v3.1/name/deutschland?fields=name,capital,population,flags,languages'
-)
-  .then(response => {
-    return response.json();
-  })
-  .then(name => {
-    console.log(name);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+  fetchCountries(name).then(countries => {});
+}
+function createMarkupList(countries) {
+  return countries
+    .map(
+      ({ name, flags }) =>
+        ` <li">
+              <img  src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <h2>${name.official}</h2></li> `
+    )
+    .join('');
+}
+function createMarkupInfo(countries) {
+  return countries
+    .map(
+      ({
+        name,
+        capital,
+        population,
+        flags,
+        languages,
+      }) => `<img src="${flags.svg}" alt="Flag of ${name}" width="320" height="auto">
+           <p> ${name.official}</p>
+            <p>Capital: <span> ${capital}</span></p>
+            <p>Population: <span> ${population}</span></p>
+            <p>Languages: <span> ${languages}</span></p>`
+    )
+    .join('');
+}
